7a30553645b1ecf93cc26d8896acbf1522196210
op30mmd <152014555+op30mmd@users.noreply.github.com>
2024-03-27 16:19:30 +0330

    update script.mjs

--- script.mjs
+++ script.mjs
@@ -14,7 +14,7 @@ async function downloadFile(url, dest) {
         file.on('finish', resolve);
         file.on('error', reject);
     });
-}
-downloadFile('https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js', '/home/runner/work/js/js')
+    downloadFile('https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js', '/home/runner/work/js/js')
     .then(() => console.log('File downloaded successfully'))
     .catch((err) => console.error('Error downloading file:', err));
+}
