5a80ab2a5beb2d54cce8b3e641ca79340c9661eb
op30mmd <152014555+op30mmd@users.noreply.github.com>
2024-03-27 15:26:19 +0330

    update script.js

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-const got = require('got');
-const fs = require('fs');
+import got from 'got';
+import fs from 'fs';
 
 async function downloadFile(url, dest) {
-    const stream = got.stream(url);
-    const file = fs.createWriteStream(dest);
+  const stream = got.stream(url);
+  const file = fs.createWriteStream(dest);
 
-    stream.pipe(file);
+  stream.pipe(file);
 
-    return new Promise((resolve, reject) => {
-        file.on('finish', resolve);
-        file.on('error', reject);
+  try {
+    await new Promise((resolve, reject) => {
+      file.on('finish', resolve);
+      file.on('error', reject);
     });
+    console.log('File downloaded successfully');
+  } catch (err) {
+    console.error('Error downloading file:', err);
+  }
 }
 downloadFile('https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.0/sweetalert.min.js', '~/')
     .then(() => console.log('File downloaded successfully'))
